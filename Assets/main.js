daa6b7adc474d49f2edda12c99b49aac5b577fd8
Dvontre Coleman <dvontrec@gmail.com>
2018-06-27 16:28:35 -0400

    Add each input

--- Assets/main.js
+++ Assets/main.js
@@ -1,19 +1,60 @@
+'use strict'
+
 const monthSelect = $('#months');
 const yearSelect = $('#year');
-const daySelect = $('#day');
+const daySelect = $('#days');
 
+const majorSelects = [monthSelect, yearSelect];
 // Creates an array of months
 const months = [ "January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December" ];
 
+let numDays = getMaxDays(monthSelect.val(),yearSelect.val());
+let days = [];
+getDays()
+
+// Adds a day up till the maximum number of days
+function getDays()
+{
+	days.splice(0,days.length);
+	for(let i = 1; i <= numDays; i++)
+	{
+		days.push(i);
+	}
+}
+
+//adds the days to day select
+days.forEach((day) =>
+{	
+	addOptions(daySelect, day, day);
+})
 // Loops through each month and adds it as an option 
-months.forEach( (month, i) => {
+months.forEach( (month, i) => 
+{
 
 	// Calls the addOptions function to add months to the month selector
 	addOptions(monthSelect, month, i+1);	
 })
 
+
 // Calls the getMax Days value whenever the month or year select is changed
-monthSelect.change( e => {console.log(getMaxDays(monthSelect.val(),yearSelect.val()))})
+majorSelects.forEach(select => 
+{
+	select.change( e =>
+	{
+		console.log(days.length)
+		getDays();
+		console.log(days.length)
+		// Removes all options 
+		daySelect.find('option')
+			.remove()
+			.end();
+		// Adds options back
+		days.forEach((day) =>
+		{	
+			addOptions(daySelect, day, day);
+		})
+	})
+});
 
 //adds the corrisponding month to each option 
 function addOptions(select, option, index)
